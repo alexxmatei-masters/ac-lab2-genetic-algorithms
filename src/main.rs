b2a9ec07284ce1b56db1e3df53ed05580d0956e8
alexxmatei <alexxmatei@gmail.com>
2023-03-09 20:40:02 +0200

    Add constants

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    const inferior_limit: u8 = 0;
+    const superior_limit: u8 = 2;
+    const number_of_individuals: u16 = 500;
+    const number_of_generations: u8 = 50;
+    const probability_crossover: f32 = 0.3;
+    const probability_mutation: f32 = 0.1;
 }
